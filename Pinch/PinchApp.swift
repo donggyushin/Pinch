b9da43c8adc579177cd6ade650f0d0945ed87383
신동규 <donggyu9410@gmail.com>
2024-08-19 15:05:42 +0900

    init project

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by 신동규 on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
